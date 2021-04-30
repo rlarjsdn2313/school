4ca79d72c61bebe07902abf63afaf0b880245a63
rlarjsdn2313 <informationdoor04@gmail.com>
2021-04-30 20:30:05 +0900

    Server:add remark on server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
+// Use express module
 const express = require('express')
 const app = express()
 
+// Set port as 4000(You may change, except 3000, it's for react)
 const port = 4000
 
 app.get('/', function(req, res) {
     res.send('Server is running...')
 })
 
+// Listening part
 app.listen(port, function() {
     console.log(`Server is running...(port:${port})`)
 })
