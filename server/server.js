c307fdee8f66d31c39bd75114d6e30e527a34196
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-01 07:37:37 +0900

    Server:setting for react js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const app = express()
 
+// Import cors to prevent error
+const cors = require('cors')
+
 // Import routes
 const check = require('./routes/check')
 const read = require('./routes/read')
@@ -10,10 +13,13 @@ const write = require('./routes/write')
 // Set port as 4000(You may change, except 3000, it's for react)
 const port = 4000
 
+// Use cors
+app.use(cors())
+
 // Set routes
-app.use('/check', check)
-app.use('/read', read)
-app.use('/write', write)
+app.use('/api/check', check)
+app.use('/api/read', read)
+app.use('/api/write', write)
 
 app.get('/', function(req, res) {
 })
