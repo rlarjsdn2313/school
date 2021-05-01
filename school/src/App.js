65e888168cc6d999e3bb4f2fdcefbcc593f89dd9
rlarjsdn2313 <informationdoor04@gmail.com>
2021-05-01 13:57:58 +0900

    React:PLZ

--- school/src/App.js
+++ school/src/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 import Login from './Components/Login'
-import Article from './Components/Article'
 
 function App() {
   const [check, setCheck] = useState('nothing')
-  if (check === 'default') {
+  console.log(check)
+  
+  if (check == 'default') {
     return (
       <div className="App">
-        <Article no="1">
-        </Article>
+        hello
       </div>
     )
+  } else {
+    return (
+      <div className="App">
+        <Login setCheck={ setCheck }></Login>
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      <Login setCheck={ setCheck }></Login>
-    </div>
-  );
+
 }
 
 export default App;
